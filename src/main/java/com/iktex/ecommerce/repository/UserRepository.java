6f92f891f6124633d9e6f9646e2f13b129d042db
fuad.qemberov <fuadqemberov50@gmail.com>
2023-12-27 16:33:11 +0400

    changes

---
+++ src/main/java/com/iktex/ecommerce/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.iktex.ecommerce.repository;
+
+import com.iktex.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
